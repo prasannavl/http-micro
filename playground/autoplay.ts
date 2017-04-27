a9717f7ee6264a6c8deaf88c9559926a5868f541
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-04-27 18:23:21 +0530

    misc: setup playground

---
+++ playground/autoplay.ts
@@ -0,0 +1,55 @@
+import * as http from "http";
+import * as micro from "http-micro";
+import * as url from "url";
+import * as util from "util";
+
+function run() {
+    let opts = {
+        host: "localhost",
+        port: 8000,
+        protocol: "http:",
+        path: "/",
+        headers: {
+        }
+    } as http.RequestOptions;
+
+    new Server().run(opts.port, opts.host, () => {
+        console.log("server running on %s:%s", opts.host, opts.port);
+        http.request(opts, (res) => {
+            console.log("-----");            
+            console.log(`${res.statusCode} ${res.statusMessage}`);
+            console.log(`${util.inspect(res.headers, false, null)}\r\n`);
+            res.on("data", (chunk) => {
+                process.stdout.write(chunk);
+            });
+            res.on("end", () => {
+                console.log("\r\n-----");
+            });
+        }).end();
+    });
+}
+
+class Server {
+    private server: micro.Application;
+
+    constructor() {
+        this.server = new micro.Application();
+        this.setupMiddleware();
+    }
+    
+    setupMiddleware() {
+        let app = this.server;
+
+        app.use((ctx, next) => {
+            const res = ctx.res;
+            ctx.sendAsJson(ctx.req.headers["some"]);
+            return Promise.resolve();
+        });
+    }
+
+    run(port: number, host = "localhost", ...args: any[]) {
+        this.server.listen(port, host, ...args);
+    }
+}
+
+run();
