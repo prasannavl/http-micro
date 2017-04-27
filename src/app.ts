26367b5f48bd9f58fffa790de411e2176c47a9b0
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-04-28 01:22:05 +0530

    add: appendHeaderValue, removeHeaderValue

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,9 @@ export class Context extends NodeContext {
     private _url: url.Url;
     private _ipAddresses: string[];
     private _routePath: string;
+
     routeHandled = false;
-    
+
     sendAsJson(data: any) {
         let res = this.res;
         this.setHeader("Content-Type", "application/json", false);
@@ -38,14 +39,90 @@ export class Context extends NodeContext {
         return true;
     }
 
-    appendHeader(key: string, value: string) {
+    /**
+     * Appends a value item to a mutli-value header key. It separates
+     * values with "," if there's an string value, or a appends to the
+     * array if there's an existing array. If none exists, creates an
+     * array with the item.
+     *
+     * @param key {String} The header key
+     * @param value {String} The header value
+     * @param forceAppend {Boolean} If true, the value will be appended
+     * regardless of whether the value is already present or not.
+     * Helpful performance optmization if it's known for certain
+     * that a value will not exist, as it avoids a regex call.
+     */
+    appendHeaderValue(key: string, value: string, forceAppend = false) {
         let res = this.res;
         let existing = res.getHeader(key);
-        if (existing) {
-            if (existing.indexOf(value) === -1)
-                res.setHeader(key, `${existing}, ${value}`);
+        if (!existing) {
+            res.setHeader(key, [value]);
+            return;
+        }
+        if (Array.isArray(existing)) {
+            if (forceAppend ||
+                !existing.includes(value)) {
+                existing.push(value);
+                res.setHeader(key, existing);
+            }
         } else {
-            res.setHeader(key, value);
+            // Header value is a string seperated by a ",".
+            let shouldSet = true;
+            if (!forceAppend) {
+                let pattern = `(?: *, *)?${value}(?: *, *)?`;
+                if (new RegExp(pattern).test(existing)) shouldSet = false;
+            }
+            if (shouldSet) res.setHeader(key, `${existing}, ${value}`);
+        }
+    }
+
+    /**
+     * Removes a value from a multi-value header item. Multi-values
+     * header can either be a string separated by ", ", or an array.
+     *
+     * Note: The value provided should one be a single string, and
+     * not an array.
+     *
+     * @param key {String} Key of the header.
+     * @param value {String} The string value to be removed from the
+     * header item.
+     * @param removeHeaderIfEmpty {Boolean} If true, removes the entire
+     * header, if the header is empty after removal of the item.
+     */
+    removeHeaderValue(key: string, value: string, removeHeaderIfEmpty = true) {
+        let res = this.res;
+        let existing = res.getHeader(key);
+        if (!existing) return;
+
+        if (Array.isArray(existing)) {
+            let arr = existing as any as Array<string>;
+            let index = arr.findIndex(x => x === value);
+            if (index > -1) {
+                arr.splice(index, 1);
+                if (removeHeaderIfEmpty && arr.length === 0) res.removeHeader(key);
+                else res.setHeader(key, arr);
+            }
+        } else {
+            
+            // Header value is a string seperated by a ",".
+            // If both the comma's are present, replace the pattern with one
+            // ", ". Or else, replace it with an empty string, and trim it.
+
+            let pattern = `( *, *)?${value}( *, *)?`;
+            let regex = new RegExp(pattern);
+            let match = existing.match(regex);
+            if (match) {
+                // If match length is 3, then it means both ", " have been
+                // matched. So, add one ",". Or else, either one of none 
+                // of the comma has been matched. It's a safe assumption
+                // to remove them entirely.
+                let v = existing
+                    .replace(match[0], match.length === 3 ? ", " : "")
+                    .trim();
+                
+                if (!v && removeHeaderIfEmpty) res.removeHeader(key);
+                else res.setHeader(key, v);
+            }
         }
     }
 
@@ -91,7 +168,7 @@ export class Context extends NodeContext {
     getHost(): string {
         let host = this.req.headers["x-forwarded-host"] || this.req.headers["host"];
         return stripBrackets(host);
-        
+
         function stripBrackets(host: string) {
             // IPv6 uses [::]:port format.
             // Brackets are used to separate the port from
@@ -110,7 +187,7 @@ export class Context extends NodeContext {
     }
 
     getProtocol(): string {
-        return  this.isEncrypted() ? "https" : "http";
+        return this.isEncrypted() ? "https" : "http";
     }
 
     isEncrypted() {
