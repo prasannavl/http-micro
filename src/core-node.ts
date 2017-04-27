0cb3434016e1965047a95186741d1f5efa37bf09
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-04-27 12:02:04 +0530

    fix: add core-node from commit

---
+++ src/core-node.ts
@@ -0,0 +1,28 @@
+import * as http from "http";
+import { Middleware, IContext, IApplication, ApplicationCore } from "./core";
+
+export class NodeApplication extends ApplicationCore<NodeContext> {
+    constructor() {
+        super(nodeContextFactory);
+    }
+}
+
+export function nodeContextFactory(app: IApplication, req: http.IncomingMessage, res: http.ServerResponse) {
+    return new NodeContext(app, req, res);
+}
+
+export class NodeContext implements IContext {
+    private _items: Map<string, any>;
+    
+    constructor(
+        public app: IApplication,
+        public req: http.IncomingMessage,
+        public res: http.ServerResponse) {
+    }
+
+    getItems() {
+        return this._items || (this._items = new Map<string, any>());
+    }
+}
+
+export type NodeMiddleware = Middleware<NodeContext>;
