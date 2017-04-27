71a342f29ad58a4049da069a997db3b0183f4199
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-04-27 12:02:15 +0530

    fix: add missing example files

---
+++ examples/example.js
@@ -0,0 +1,78 @@
+const micro = require("http-micro");
+const url = require("url");
+const mount = micro.mount;
+
+let app = new micro.Application();
+// When using Typescript, context can be 
+// generically typed to one that implements
+// the IContext interface.
+// `let app = new micro.ApplicationCore<MyContext>();`
+
+// Raw node req, and res untouched.
+// Convenience functions are also provided, used
+// later in the example.
+
+app.use(async (ctx, next) => {
+    if (url.parse(ctx.req.url)
+        .pathname == "/async") {
+        ctx.res.end("Hello world from async!");
+    } else {
+        await next();
+    }
+});
+
+app.use(async (ctx, next) => {
+    await next();
+})
+
+app.use(async (ctx, next) => {
+    if (url.parse(ctx.req.url)
+        .pathname == "/async-await") {
+        await Promise.resolve();
+        await Promise.resolve();
+        ctx.res.end("Hello world from awaited async!");        
+    } else {
+        await next();
+    }
+});
+
+let router = new micro.Router();
+// When using Typescript, can again be generic,
+// `let router = new Router<MyContext>();`
+
+router.get("/hello", (ctx) => {
+    ctx.sendText("Hello route!");
+    return Promise.resolve();
+});
+
+router.get("/hello-string", async (ctx) => {
+    ctx.sendText("Hello string!");
+});
+
+router.get("/hello-object", (ctx) => {
+    ctx.sendAsJson({ message: "Hello world!" });
+    return Promise.resolve();
+});
+
+let router1 = new micro.Router();
+
+router1.get("/hello", (ctx) => {
+    ctx.sendText("chain 1: hello!");
+    return Promise.resolve();
+});
+
+let router2 = new micro.Router();
+
+router2.get("/hello", (ctx) => {
+    ctx.res.end("chain 2: hello!");
+    return Promise.resolve();
+});
+
+router.use(mount("/r1", router1, "router1"));
+router.use(mount("/r2/", router2, "router2"));
+
+app.use(mount("/", router, "root-router"));
+
+app.listen(8000, "localhost", () => {
+    console.log("listening");
+});
---
+++ examples/example2.ts
@@ -0,0 +1,111 @@
+import * as http from "http";
+import * as path from "path";
+import { Middleware, ApplicationCore, Context, IContext, Router, mount } from "http-micro";
+import * as url from "url";
+import * as os from "os";
+
+class AppContext extends Context {}
+
+export class Server {
+    private server: ApplicationCore<AppContext>;
+
+    constructor() {
+        this.server = new ApplicationCore<AppContext>(
+            (app, req, res) => new AppContext(app, req, res));
+        this.setupMiddleware();
+    }
+
+    setupMiddleware() {
+        let app = this.server;
+
+        app.use((ctx, next) => {
+            if (ctx.req.url.endsWith("raw")) {
+                ctx.res.end("Hello from raw middlware!");
+                return Promise.resolve();
+            } else {
+                return next();
+            }
+        });
+
+        app.use(mount("/api", (ctx, next) => {
+            ctx.sendAsJson({
+                message: "api route!",
+                routePath: ctx.getRoutePath(),
+                path: ctx.getUrl(),
+            });
+            return Promise.resolve();
+        }, "api"));
+
+        app.use(mount("/", this.getRouter(), "root"));
+        app.use(mount("/chain/", this.getRouterChain(), "chain"));
+
+        app.use(async (ctx, next) => {
+            ctx.res.end("Not found");
+        });
+    }
+
+    private getRouter() {
+        let router = new Router<AppContext>();
+
+        router.get("/hello", (ctx, next) => {
+            ctx.sendText("Hello route!");
+            return Promise.resolve();
+        });
+
+        router.get("/hello-string", (ctx, next) => {
+            ctx.sendText("Hello string!");
+            return Promise.resolve();
+        });
+
+        router.get("/hello-object", (ctx, next) => {
+            ctx.sendAsJson({ message: "Hello world!" });
+            return Promise.resolve();
+        });
+
+        // This will not work since it's taken over by the api mount.        
+        router.get("/api/numbers", (ctx, next) => {
+            let str = "";
+            for (let i = 0; i < 1000; i++) {
+                str += i.toString() + "\n";
+            }
+            ctx.res.end(str);
+            return Promise.resolve();
+        });
+
+        return router;
+    }
+
+    private getRouterChain() {
+        let router = new Router<AppContext>();
+
+        router.get("/hello", (ctx, next) => {
+            ctx.res.end("chain 0: hello!");
+            return Promise.resolve();
+        });
+
+        let router2 = new Router<AppContext>();
+
+        router2.get("/hello", (ctx, next) => {
+            ctx.res.end("chain 1: hello!");
+            return Promise.resolve();
+        });
+
+        let router3 = new Router<AppContext>();
+
+        router3.get("/hello", (ctx, next) => {
+            ctx.res.end("chain 2: hello!");
+            return Promise.resolve();
+        });
+
+        router.use(mount("/c1", router2, "router2"));
+        router.use(mount("/c2/", router3, "router3"));
+
+        return router;
+    }
+
+    run(port: number, host = "localhost") {
+        this.server.listen(port, host, () => {
+            console.log("server listening on %s:%s", host, port);
+        });
+    }
+}
