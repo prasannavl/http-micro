471ac28e7f1032f3bf6e7c61ce66731b8838068f
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-05-21 22:32:21 -0300

    remove: hasRequestBody - use typeis

--- src/context.ts
+++ src/context.ts
@@ -72,21 +72,6 @@ export class Context extends NodeContext {
         this.send(reason, null, 401);
     }
 
-    /**
-     * Check if a request has a request body.
-     * A request with a body __must__ either have `transfer-encoding`
-     * or `content-length` headers set.
-     * http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.3
-     *
-     * @param {Object} request
-     * @return {Boolean}
-     */
-    hasRequestbody() {
-        let req = this.req;
-        return req.headers['transfer-encoding'] !== undefined ||
-            !isNaN(req.headers['content-length'])
-    }
-
     setHeader(key: string, value: string, replace = true) {
         let res = this.res;
         if (!replace && res.getHeader(key)) return false;
