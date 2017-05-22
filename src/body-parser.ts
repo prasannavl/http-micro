739b978f6b6fd38cf4eda1153ae65eb3c3174350
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-05-21 22:38:49 -0300

    add: primitive body parsers

--- src/body-parser.ts
+++ src/body-parser.ts
@@ -1,12 +1,16 @@
 import * as http from "http";
 import * as rawBody from "raw-body";
 import * as qs from "querystring";
+import * as typeis from "type-is";
+import * as httpError from "http-errors";
+import { IContext } from "./core";
 
 export type ParserCallback = (error: rawBody.RawBodyError, body?: string) => void;
+export type Parser = (req: http.IncomingMessage, res: http.ServerResponse, callback: ParserCallback) => void;
 const defaultLimit = 1024 * 1024 / 2; // 512Kb
 
-export function rawBodyParserFactory(req: http.IncomingMessage, res: http.ServerResponse, opts: rawBody.Options) {
-    return function rawParser(callback: ParserCallback) {
+export function rawBodyParserFactory(opts: rawBody.Options) {
+    return function rawParser(req: http.IncomingMessage, res: http.ServerResponse, callback: ParserCallback) {
 
         var limit = opts.limit || defaultLimit;
         var contentLength = req.headers ?
@@ -21,14 +25,14 @@ export function rawBodyParserFactory(req: http.IncomingMessage, res: http.Server
     };
 }
 
-export interface JsonBodyParserOpts extends rawBody.Options { reviver?: (key: any, value: any) => any };
+export type JsonBodyParserOpts = rawBody.Options & { reviver?: (key: any, value: any) => any };
 
-export function jsonBodyParserFactory(req: http.IncomingMessage, res: http.ServerResponse, opts: JsonBodyParserOpts) {
-    let rawParser = rawBodyParserFactory(req, res, opts);
+export function jsonBodyParserFactory(opts: JsonBodyParserOpts, defaultParser?: Parser) {
+    let rawParser = defaultParser || rawBodyParserFactory(opts);
     let reviver = opts.reviver;
 
-    return function jsonParser(callback: ParserCallback) {
-        rawParser(function (err, body) {
+    return function jsonParser(req: http.IncomingMessage, res: http.ServerResponse, callback: ParserCallback) {
+        rawParser(req, res, function (err, body) {
             if (err) {
                 return callback(err);
             }
@@ -43,22 +47,22 @@ export function jsonBodyParserFactory(req: http.IncomingMessage, res: http.Serve
     };
 }
 
-export interface FormBodyParserOpts extends rawBody.Options {
+export type FormBodyParserOpts = rawBody.Options & {
     parser?: (str: string, sep?: string, eq?: string, options?: qs.ParseOptions) => any;
     sep?: string;
     eq?: string;
     options?: qs.ParseOptions;
 };
 
-export function formBodyParserFactory(req: http.IncomingMessage, res: http.ServerResponse, opts: FormBodyParserOpts) {
-    let rawParser = rawBodyParserFactory(req, res, opts);
+export function formBodyParserFactory(opts: FormBodyParserOpts, defaultParser?: Parser) {
+    let rawParser = defaultParser || rawBodyParserFactory(opts);
     let qsParse = opts.parser || qs.parse;
     let sep = opts.sep;
     let eq = opts.eq;
     let options = opts.options;
 
-    return function jsonParser(callback: ParserCallback) {
-        rawParser(function (err, body) {
+    return function jsonParser(req: http.IncomingMessage, res: http.ServerResponse, callback: ParserCallback) {
+        rawParser(req, res, function (err, body) {
             if (err) {
                 return callback(err);
             }
@@ -71,4 +75,64 @@ export function formBodyParserFactory(req: http.IncomingMessage, res: http.Serve
             callback(null, res);
         });
     };
+}
+
+export type AnyParserOptions = FormBodyParserOpts & JsonBodyParserOpts;  
+
+export function anyBodyParserFactory(opts: AnyParserOptions, defaultParser?: Parser) {
+    let rawParser = defaultParser || rawBodyParserFactory(opts);
+    let jsonParser = jsonBodyParserFactory(opts, rawParser);
+    let formParser = formBodyParserFactory(opts, rawParser);
+    let types = ["json", "urlencoded"];
+
+    return function anyBodyParser(req: http.IncomingMessage, res: http.ServerResponse, callback: ParserCallback) {
+        let t = typeis(req, types);
+        switch (t) {
+            case types[0]: {
+                jsonParser(req, res, callback);
+                break;
+            }
+            case types[1]: {
+                formParser(req, res, callback);
+                break;
+            }
+            default: {
+                rawParser(req, res, callback);
+            }
+        }
+    };
+}
+
+export function parserFactory(opts: AnyParserOptions) {
+    let parser = anyBodyParserFactory(opts);
+    return function parse(context: IContext): Promise<any> {
+        let req = context.req;
+        let res = context.res;
+        return new Promise((resolve, reject) => {
+            parser(req, res, (err, body) => {
+                if (err) {
+                    reject(new httpError.BadRequest(err.message));
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+    }
+}
+
+export function createAsyncParser(parser: Parser = anyBodyParserFactory({}), errorExpose = false) {
+    return function parse(context: IContext): Promise<any> {
+        let req = context.req;
+        let res = context.res;
+        return new Promise((resolve, reject) => {
+            parser(req, res, (err, body) => {
+                if (err) {
+                    let errObj = errorExpose ? new httpError.BadRequest(err.message) : new httpError.BadRequest();
+                    reject(errObj);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+    }
 }
