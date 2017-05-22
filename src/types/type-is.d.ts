b709578ada3c61cf9b098a08e24dbd78ea36a2ba
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-05-21 22:38:38 -0300

    add: typeis declarations

---
+++ src/types/type-is.d.ts
@@ -0,0 +1,12 @@
+declare module "type-is" {
+    import * as http from "http";
+
+    namespace TypeIs {
+        function hasBody(request: http.IncomingMessage): boolean;
+        function is(mediaType: string, mediaTypes: Array<string>): string;
+    }
+
+    function TypeIs(request: http.IncomingMessage, types: Array<string>): any;
+    export = TypeIs;
+}
+
