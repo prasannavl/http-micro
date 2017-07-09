2eb30283480d88211e5843201e7058650f0185eb
Prasanna V. Loganathar <pvl@prasannavl.com>
2017-07-09 11:15:03 +0530

    fix: body parser default form handling, enhance: error handling

--- src/body-parser.ts
+++ src/body-parser.ts
@@ -2,35 +2,39 @@ import * as http from "http";
 import * as rawBody from "raw-body";
 import * as qs from "querystring";
 import * as typeis from "type-is";
-import * as httpError from "http-errors";
 import * as contentType from "content-type";
 import * as mimeTypes from "mime-types";
-import { isHttpError, wrapError } from "./error-utils";
+import { intoHttpError } from "./error-utils";
 
 export type ParserCallback = (error: rawBody.RawBodyError, body?: string) => void;
 export type Parser = (req: http.IncomingMessage, callback: ParserCallback) => void;
 const defaultLimit = 1024 * 1024 / 2; // 512Kb
 
-export function rawBodyParserFactory(opts: rawBody.Options) {
-    return function rawParser(req: http.IncomingMessage, callback: ParserCallback, state?: any) {
+export function rawBodyParserFactory() {
+    return function rawParser(req: http.IncomingMessage, callback: ParserCallback, opts?: any) {
         if (handleRequestBodyAbsence(req, callback)) return;
 
-        var limit = opts.limit || defaultLimit;
-        var contentLength = opts.length || Number(req.headers["content-length"]);
+        let limit = opts.limit || defaultLimit;
+        let contentLength = opts.length || Number(req.headers["content-length"]);
         let encoding = opts.encoding;
+
         if (encoding === undefined) {
             let contentTypeHeader = req.headers["content-type"];
             // Ensure that further attempts are skipped, as contentType
             // will throw on invalid header. Since rawParser could 
             // potentially be passed on it's own to get a buffer back.
             if (contentTypeHeader) {
-                let ct = contentType.parse(contentTypeHeader);
-                encoding = ct.parameters["charset"] as any;
-                if (!encoding) {
-                    // No valid encoding was found, but content-type
-                    // header is valid. So, pick up the default 
-                    // encoding for the mime.
-                    encoding = mimeTypes.charset(ct.type) as string;
+                try {
+                    let ct = contentType.parse(contentTypeHeader);
+                    encoding = ct.parameters["charset"] as any;
+                    if (!encoding) {
+                        // No valid encoding was found, but content-type
+                        // header is valid. So, pick up the default 
+                        // encoding for the mime.
+                        encoding = mimeTypes.charset(ct.type) as string;
+                    }
+                } catch (err) {
+                    throw intoHttpError(err, 400);
                 }
             }
         }
@@ -45,11 +49,11 @@ export function rawBodyParserFactory(opts: rawBody.Options) {
 
 export type JsonBodyParserOpts = rawBody.Options & { reviver?: (key: any, value: any) => any };
 
-export function jsonBodyParserFactory(opts: JsonBodyParserOpts, defaultParser?: Parser) {
-    let rawParser = defaultParser || rawBodyParserFactory(opts);
+export function jsonBodyParserFactory(opts: JsonBodyParserOpts, baseParser?: Parser) {
+    let rawParser = baseParser || rawBodyParserFactory();
     let reviver = opts.reviver;
 
-    return function jsonParser(req: http.IncomingMessage, callback: ParserCallback, state?: any) {
+    return function jsonParser(req: http.IncomingMessage, callback: ParserCallback, baseParserOpts?: any) {
         rawParser(req, function (err, body) {
             if (err) {
                 return callback(err);
@@ -61,7 +65,7 @@ export function jsonBodyParserFactory(opts: JsonBodyParserOpts, defaultParser?: 
                 return callback(e);
             }
             callback(null, res);
-        });
+        }, baseParserOpts);
     };
 }
 
@@ -72,14 +76,18 @@ export type FormBodyParserOpts = rawBody.Options & {
     options?: qs.ParseOptions;
 };
 
-export function formBodyParserFactory(opts: FormBodyParserOpts, defaultParser?: Parser) {
-    let rawParser = defaultParser || rawBodyParserFactory(opts);
+export function formBodyParserFactory(opts: FormBodyParserOpts, baseParser?: Parser) {
+    let rawParser = baseParser || rawBodyParserFactory();
     let qsParse = opts.parser || qs.parse;
     let sep = opts.sep;
     let eq = opts.eq;
     let options = opts.options;
 
-    return function jsonParser(req: http.IncomingMessage, callback: ParserCallback, state?: any) {
+    return function formBodyParser(req: http.IncomingMessage, callback: ParserCallback, baseParserOpts?: any) {
+        let baseOpts = baseParserOpts as rawBody.Options;
+        if (!baseOpts || !baseOpts.encoding) {
+            baseOpts = Object.assign({}, baseOpts, { encoding: true });
+        }
         rawParser(req, function (err, body) {
             if (err) {
                 return callback(err);
@@ -91,33 +99,33 @@ export function formBodyParserFactory(opts: FormBodyParserOpts, defaultParser?: 
                 return callback(e);
             }
             callback(null, res);
-        });
+        }, baseOpts);
     };
 }
 
 export type AnyParserOptions = FormBodyParserOpts & JsonBodyParserOpts;  
 
-export function anyBodyParserFactory(opts: AnyParserOptions, defaultParser?: Parser) {
-    let rawParser = defaultParser || rawBodyParserFactory(opts);
+export function anyBodyParserFactory(opts: AnyParserOptions, baseParser?: Parser) {
+    let rawParser = baseParser || rawBodyParserFactory();
     let jsonParser = jsonBodyParserFactory(opts, rawParser);
     let formParser = formBodyParserFactory(opts, rawParser);
     let types = ["json", "urlencoded"];
     
-    return function anyBodyParser(req: http.IncomingMessage, callback: ParserCallback, state?: any) {
+    return function anyBodyParser(req: http.IncomingMessage, callback: ParserCallback, baseParserOpts?: any) {
         if (handleRequestBodyAbsence(req, callback)) return;
 
         let t = typeis(req, types);
         switch (t) {
             case types[0]: {
-                jsonParser(req, callback);
+                jsonParser(req, callback, baseParserOpts);
                 break;
             }
             case types[1]: {
-                formParser(req, callback);
+                formParser(req, callback, baseParserOpts);
                 break;
             }
             default: {
-                rawParser(req, callback);
+                rawParser(req, callback, baseParserOpts);
             }
         }
     };
@@ -128,14 +136,7 @@ export function createAsyncParser(parser: Parser) {
         return new Promise((resolve, reject) => {
             parser(req, (err, body) => {
                 if (err) {
-                    let errObj;
-                    if (isHttpError(err)) {
-                        errObj = err;
-                    } else {
-                        errObj = new httpError.BadRequest();
-                        wrapError(errObj, err);
-                    }
-                    reject(errObj);
+                    reject(intoHttpError(err, 400));
                 } else {
                     resolve(body);
                 }
